2318fe76332dd4e02383571646acc15ac447a2d1
Stanislav Ivanov <stanislav.ivanov@jetbrains.com>
2024-08-27 18:11:06 +0200

    opt-in for JsExport

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -20,15 +20,19 @@ kotlin {
     }
 
     sourceSets {
-        commonMain.dependencies {
-            implementation(projects.shared)
+        commonMain {
+            languageSettings.optIn("kotlin.js.ExperimentalJsExport")
 
-            implementation(libs.ktor.client.js)
-            implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.websockets)
-            implementation(libs.kotlinx.rpc.krpc.client)
-            implementation(libs.kotlinx.rpc.krpc.serialization.json)
-            implementation(libs.kotlinx.rpc.krpc.ktor.client)
+            dependencies {
+                implementation(projects.shared)
+
+                implementation(libs.ktor.client.js)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.websockets)
+                implementation(libs.kotlinx.rpc.krpc.client)
+                implementation(libs.kotlinx.rpc.krpc.serialization.json)
+                implementation(libs.kotlinx.rpc.krpc.ktor.client)
+            }
         }
     }
 }
