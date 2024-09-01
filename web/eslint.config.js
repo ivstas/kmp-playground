e6adc907ee72cf50685180e8a3552b224200761f
Stanislav Ivanov <stanislav.ivanov@jetbrains.com>
2024-09-01 03:07:47 +0200

    add eslint stylistic

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,17 +1,23 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
+import typescriptEslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
-   {files: ['**/*.{js,mjs,cjs,ts}']},
-   {languageOptions: { globals: globals.browser }},
+   { files: ['**/*.{js,mjs,cjs,ts}'] },
+   { languageOptions: { globals: globals.browser } },
    pluginJs.configs.recommended,
-   ...tseslint.configs.recommended,
+   ...typescriptEslint.configs.recommended,
    {
+      plugins: {
+         '@stylistic': stylistic,
+      },
       rules: {
          'indent': ['warn', 3],
-         'quotes': ['warn', 'single']
+         'object-curly-spacing': ['warn', 'always'],
+         'no-unused-vars': 'warn',
+         '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+         'quotes': ['warn', 'single'],
       },
-   }
+   },
 ];
