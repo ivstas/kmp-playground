2910284780a0c79f332687234a09927d9fa11bb8
Stanislav Ivanov <stanislav.ivanov@jetbrains.com>
2024-09-29 15:22:07 +0200

    fix eslint config

---
+++ web/eslint.config.mjs
@@ -0,0 +1,29 @@
+import pluginJs from '@eslint/js';
+import typescriptEslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin'
+import react from 'eslint-plugin-react'
+
+export default typescriptEslint.config({
+   files: ['src/**/*.{js,mjs,cjs,ts}'],
+   extends: [
+      pluginJs.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+   ],
+   languageOptions: {
+      parserOptions: {
+         project: true,
+      },
+   },
+   plugins: {
+      '@stylistic': stylistic,
+      'react': react,
+   },
+   rules: {
+      'indent': ['warn', 3],
+      'object-curly-spacing': ['warn', 'always'],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/switch-exhaustiveness-check': 'error',
+      '@stylistic/comma-dangle': ['warn', 'always-multiline'],
+      'quotes': ['warn', 'single'],
+   },
+})
